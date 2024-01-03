ec55da64c1f7152b7eb7fefc11ea9dcbeceb8fd7
Engesvold, Martin <Martin.Engesvold@nordicsemi.no>
2024-01-03 15:18:56 +0100

    Modulized kickstarter config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,63 @@
+-- Enable the following language servers
+--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+--
+--  Add any additional override configuration in the following tables. They will be passed to
+--  the `settings` field of the server config. You must look up that documentation yourself.
+--
+--  If you want to override the default filetypes that your language server will attach to you can
+--  define the property 'filetypes' to the map in question.
+local servers = {
+  -- clangd = {},
+  -- gopls = {},
+  -- pyright = {},
+  -- rust_analyzer = {},
+  -- tsserver = {},
+  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+
+  lua_ls = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+      -- diagnostics = { disable = { 'missing-fields' } },
+    },
+  },
+}
+
+return {
+  {
+    'williamboman/mason.nvim',
+    config = function ()
+      require('mason').setup()
+    end
+  },
+-- mason-lspconfig requires that these setup functions are called in this order
+-- before setting up the servers.
+  {
+    'williamboman/mason-lspconfig.nvim',
+    config = function ()
+      require('mason-lspconfig').setup()
+    end
+  },
+  -- NOTE: This is where your plugins related to LSP can be installed.
+  --  The configuration is done below. Search for lspconfig to find it below.
+  {
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      { 'williamboman/mason.nvim', config = true },
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim', opts = {} },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+    },
+    config = function()
+
+    end
+  },
+}
---
+++ lua/plugins/theme-onedark.lua
@@ -0,0 +1,9 @@
+return {
+  -- Theme inspired by Atom
+  'navarasu/onedark.nvim',
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme 'onedark'
+  end,
+}
+-- vim: ts=2 sts=2 sw=2 et
